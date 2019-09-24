6b7d4c863000f843c3af7d1adfed1a53f8b5017a
Matt Eding <matteding@gmail.com>
2019-09-23 23:22:16 -0700

    reshape COO; sparse __str__; change idxs.T to idxs

--- ndcontainers/sparse/base.py
+++ ndcontainers/sparse/base.py
@@ -8,9 +8,14 @@ __all__ = ['SparseArrayABC']
 class SparseArrayABC(abc.ABC):
     @abc.abstractmethod
     def __array__(self):
-        return 
+        raise NotImplementedError 
 
-    
+    def __str__(self):
+        return (
+            f"<{type(self).__name__}: shape={self.shape}, nnz={self.nnz}, "
+            f"dtype={self.dtype}, fill_value={self.fill_value}>"
+        )
+
     @property
     def dtype(self):
         return self.data.dtype
@@ -20,9 +25,9 @@ class SparseArrayABC(abc.ABC):
         return len(self.shape)
 
     @property
+    def nnz(self):
+        return len(self.data)
+
+    @property
     def size(self):
         return np.prod(self.shape, dtype=int)
-
-    # @abc.abstractmethod
-    # def reshape(self, shape, order='C'):
-    #     pass
--- ndcontainers/sparse/coordinate.py
+++ ndcontainers/sparse/coordinate.py
@@ -13,10 +13,10 @@ class CoordinateArray(
 ):
     def __init__(self, data, idxs, shape, fill_value=0, dtype=None, copy=False):
         self.data = np.array(data, dtype=dtype, copy=copy)
-        self.idxs = np.atleast_2d(np.array(idxs, dtype=int, copy=copy))
-        self.shape = shape
+        self.idxs = np.atleast_2d(np.array(idxs, dtype=np.uint, copy=copy))
+        self.shape = tuple(shape)
         self.fill_value = fill_value
-        
+
         # assert (self.data.ndim == 1) or (not self.data.size)
         # assert (self.idxs.ndim == 2) or (not self.idxs.size)
         # if not self.data.size:
@@ -41,12 +41,12 @@ class CoordinateArray(
 
         return self._repr_(
             data, idxs, self.shape, self.fill_value, self.dtype,
-            ignore=('copy')
+            ignore=('copy',)
         )
 
     def __array__(self):
         arr = np.full(self.shape, fill_value=self.fill_value, dtype=self.dtype)
-        arr[tuple(self.idxs.T)] = self.data
+        arr[tuple(self.idxs)] = self.data
         return arr
 
     def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
@@ -80,3 +80,24 @@ class CoordinateArray(
             return type(self)(ufunc(*scalars, **kwargs), idxs, shape, )
         else:
             return NotImplemented
+
+    def reshape(self, *shape, copy=True, order='C'):
+        shape = np.array(shape)
+        neg = np.flatnonzero(shape < 0)
+        if neg.any():
+            if len(neg) > 1:
+                raise ValueError("can only specify one unknown dimension")
+            shape[neg] = self.size // -np.prod(shape)
+
+        if shape.prod() != self.size:
+            raise ValueError(
+                f"cannot reshape {type(self).__name__} of "
+                f"size {self.size} into shape {shape}"
+            )
+
+        raveled = np.ravel_multi_index(self.idxs, self.shape, order=order)
+        unraveled = np.unravel_index(raveled, shape, order=order)
+        idxs = np.array(unraveled, dtype=np.uint)
+        return type(self)(
+            self.data, idxs, shape, self.fill_value, self.dtype, copy=copy
+        )
