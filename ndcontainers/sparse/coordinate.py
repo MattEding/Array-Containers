6ff942fca48a017e9d1141c4daed8703e7b81e46
Matt Eding <matteding@gmail.com>
2019-10-01 22:25:48 -0700

    ravel_sparse_multi_index input checks; basic coo indexing

--- ndcontainers/sparse/coordinate.py
+++ ndcontainers/sparse/coordinate.py
@@ -7,36 +7,27 @@ import numpy as np
 from ..abc import SparseArray
 from ..utils import NDArrayReprMixin
 from ..utils import is_broadcastable
+from ..utils import ravel_sparse_multi_index
+from ..utils import unravel_sparse_index
 
 
 __all__ = ['CoordinateArray']
-
-# FIXME: if ndim > 32 -> raise Warning in init -- ndarray doesn't support 33+
-#           error arises during reshape and __array__
 
 
 class CoordinateArray(
     np.lib.mixins.NDArrayOperatorsMixin, NDArrayReprMixin, SparseArray
 ):
     # is there a real advantage to idxs *before* data? if so change order
-    def __init__(self, data, idxs, shape=None, fill_value=None, dtype=None, copy=False, sum_duplicates=True):
-        self._idxs = np.atleast_2d(np.array(idxs, dtype=np.uint, copy=copy))
-        if self.idxs.ndim != 2:
-            raise ValueError(f"'idxs' must be 2d, not {self.idxs.ndim}d")
-
-        #TODO: if shape=() then data should be scalar array of value = fill_value
-        if not np.iterable(data):
-            data = tuple(itertools.repeat(data, self._idxs.shape[-1]))
-
-        self._data = np.array(data, dtype=dtype, copy=copy)
-        if self.data.ndim != 1:
-            raise ValueError(f"'data' must be 1d, not {self.data.ndim}d")
-
-        if self.data.shape[-1] != self.idxs.shape[-1]:
-            raise ValueError("'data' does not have 1-1 correspondence with 'idxs'")
+    def __init__(
+        self, data, idxs, shape=None, fill_value=None, dtype=None,
+        copy=False, sum_duplicates=True
+    ):
+        multi_idx = np.atleast_2d(np.array(idxs, dtype=np.uint, copy=copy))
+        if multi_idx.ndim != 2:
+            raise ValueError(f"'idxs' must be 2d, not {multi_idx.ndim}d")
 
         try:
-            min_shape = self.idxs.max(axis=1) + 1
+            min_shape = multi_idx.max(axis=1) + 1
         except ValueError:
             min_shape = ()
 
@@ -51,12 +42,27 @@ class CoordinateArray(
             else:
                 self._shape = tuple(shape)
 
+        self._idxs = ravel_sparse_multi_index(multi_idx, self.shape)
+
+        #TODO: if shape=() then data should be scalar array of value = fill_value
+        if not np.iterable(data):
+            data = tuple(itertools.repeat(data, self._idxs.shape[-1]))
+
+        self._data = np.array(data, dtype=dtype, copy=copy)
+        if self.data.ndim != 1:
+            raise ValueError(f"'data' must be 1d, not {self.data.ndim}d")
+
+        if self.data.shape[-1] != self.idxs.shape[-1]:
+            raise ValueError("'data' does not have 1-1 correspondence with 'idxs'")
+
         if fill_value is None:
             fill_value = np.zeros((), self.dtype).item()
         self.fill_value = fill_value
 
         if sum_duplicates:
             self.sum_duplicates()
+        else:
+            self.sort_index()
 
     def __repr__(self):
         data = np.array2string(
@@ -66,7 +72,7 @@ class CoordinateArray(
         )
 
         idxs = np.array2string(
-            self.idxs,
+            np.vstack(unravel_sparse_index(self.idxs, self.shape)),
             separator=", ",
             prefix=self._prefix_(1),
         )
@@ -77,7 +83,18 @@ class CoordinateArray(
         )
 
     def __getitem__(self, idx):
-        return ...
+        # TODO: slicing & fancy indexing & masks
+        try:
+            ravel = ravel_sparse_multi_index(idx, self.shape)
+        except ValueError:
+            raise IndexError('...')
+            # IndexError: index 9 is out of bounds for axis 0 with size 4
+            # IndexError: too many indices for array
+        loc = self.idxs.searchsorted(ravel)
+        if self.idxs[loc] == ravel:
+            return self.data[loc]
+        else:
+            return self.fill_value
 
     def __eq__(self, other):
         if not is_broadcastable(self, other):
@@ -136,8 +153,7 @@ class CoordinateArray(
 
     def __array__(self):
         arr = np.full(self.shape, fill_value=self.fill_value, dtype=self.dtype)
-        raveled_idxs = np.ravel_multi_index(self.idxs, (self.shape or 0))
-        arr.put(raveled_idxs, self.data)
+        arr.ravel()[self._idxs] = self.data
         return arr
 
     def __array_ufunc__(self, ufunc, method, *inputs, **kwargs):
@@ -147,8 +163,9 @@ class CoordinateArray(
 
     def astype(self, dtype, casting='unsafe', copy=True):
         data = self._data.astype(dtype, casting=casting, copy=copy)
+        unraveled = np.vstack(unravel_sparse_index(self.idxs, self.shape))
         if copy:
-            return type(self)(data, self.idxs, self.shape, self.fill_value, copy=copy)
+            return type(self)(data, unraveled, self.shape, self.fill_value, copy=copy)
         else:
             self._data = data
             return self
@@ -158,12 +175,20 @@ class CoordinateArray(
         if not shape.size:
             # TODO: copy=copy
             return self
-        raveled = np.ravel_multi_index(self.idxs, self.shape)
-        unraveled = np.unravel_index(raveled, shape)
-        idxs = np.array(unraveled, dtype=np.uint)
+
+        unraveled = np.unravel_index(self._idxs, shape)
+
         return type(self)(
-            self.data, idxs, shape, self.fill_value, self.dtype, copy=copy
+            self.data, unraveled, shape, self.fill_value, self.dtype, copy=copy
         )
+
+    def sort_index(self):
+        """Sort indices and and their respective data values.
+        This is an in-place operation.
+        """
+        sorter = self.idxs.argsort()
+        self._idxs = self.idxs[sorter]
+        self._data = self.data[sorter]
 
     def sum_duplicates(self):
         """Eliminate duplicate entries by adding them together.
@@ -172,8 +197,8 @@ class CoordinateArray(
         if not self.size:
             return
 
-        self._idxs, inverse = np.unique(self.idxs, axis=1, return_inverse=True)
-        data = np.zeros_like(self.idxs[0], dtype=self.dtype)
+        self._idxs, inverse = np.unique(self.idxs, return_inverse=True)
+        data = np.zeros_like(self.idxs, dtype=self.dtype)
         np.add.at(data, inverse, self.data)
         self._data = data
 
